9ae173e27ebecc1d0c8900bb701a4a889d439002
ophid48 <33008283+ophid48@users.noreply.github.com>
2019-05-02 15:37:10 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,52 +1,56 @@
+#include <SFML/Graphics.hpp>
 #include <iostream>
-#include <random>
-#include <vector>
-
-using namespace std;
-
-bool end_game;
-
-class Game
-{
-private:
-	const int width = { 20 }, heigth = { 20 };
-
-	int x, y;
-public:
-	int wolf_M, wolf_F, rabbits;
-	Game() : wolf_M(1), wolf_F(1), rabbits(1) {}
-	Game(int w1, int w2, int r) : wolf_M(w1), wolf_F(w1), rabbits(r) {}
-	void setup() {
-		end_game = false;
-
-	}
-	void draw() {
-		system("cls");
-		for (int i = 0; i < width; i++)
-			cout << '#';
-		cout << endl;
-
-		for (int i = 0; i < heigth; i++) {
-			for (int j = 0; j < width; j++) {
-				if (j == 0 || j == width - 1)
-					cout << '#';
-				else cout << ' ';
-			}
-			cout << endl;
-		}
-
-		for (int i = 0; i < width; i++)
-			cout << '#';
-
-	}
-}game;
-
+using namespace sf;
 
 int main()
 {
-	game.setup();
-	while (!end_game) {
-		game.draw();
+	sf::RenderWindow window(sf::VideoMode(1000, 1050), "Island");
+	Texture texture;
+	Texture texture1;
+
+	texture.loadFromFile("images/background.png");
+	Sprite background;
+	background.setTexture(texture);
+
+	texture1.loadFromFile("images/rabbit.png");
+	Sprite rabbit;
+	rabbit.setTexture(texture1);
+	rabbit.setPosition(Vector2f(1, 51));
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::KeyPressed &&
+				event.key.code == sf::Keyboard::Escape ||
+				event.type == event.Closed)
+				window.close();
+			if (event.type == Event::KeyPressed &&
+				event.key.code == Keyboard::W   &&
+				rabbit.getPosition().y>51 )
+				rabbit.move(0, -50);
+			if (event.type == Event::KeyPressed &&
+				event.key.code == Keyboard::S &&
+				rabbit.getPosition().y < 952)
+				rabbit.move(0, 50);
+			if (event.type == Event::KeyPressed &&
+				event.key.code == Keyboard::A && 
+				rabbit.getPosition().x > 1)
+				rabbit.move(-50, 0);
+			if (event.type == Event::KeyPressed &&
+				event.key.code == Keyboard::D && 
+				rabbit.getPosition().x < 951)
+				rabbit.move(50, 0);
+		}
+
+		
+
+		window.clear();
+		window.draw(background);
+		window.draw(rabbit);
+		window.display();
 	}
 
+	return 0;
 }
